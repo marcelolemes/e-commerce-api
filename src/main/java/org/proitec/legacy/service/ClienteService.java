f7d8bd31f413265ae502c0c2e0cd4f4a6f8dca4f
Marcelo Lemes da Silveira <lemmarcelo@gmail.com>
2022-10-31 20:59:06 -0300

    FEAT inclusao de services

---
+++ src/main/java/org/proitec/legacy/service/ClienteService.java
@@ -0,0 +1,39 @@
+package org.proitec.legacy.service;
+
+import org.proitec.legacy.model.Cliente;
+import org.proitec.legacy.model.Transacao;
+import org.proitec.legacy.repository.ClienteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@ApplicationScoped
+public class ClienteService {
+    Logger log = LoggerFactory.getLogger(ClienteService.class);
+    @Inject
+    ClienteRepository clienteRepository;
+
+
+    @Transactional
+    public boolean adicionarComercio(Cliente cliente) {
+        clienteRepository.persist(cliente);
+        return true;
+    }
+
+    public List<?> buscarPorNome(String nome) {
+        return clienteRepository.findByNome(nome);
+    }
+
+    public List<?> buscarPorCpf(String cpf) {
+        return clienteRepository.findByCpf(cpf);
+    }
+
+    public List<?> listAll() {
+        return clienteRepository.listAll();
+    }
+}
